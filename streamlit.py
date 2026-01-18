2425a0c805e29999ec22d62c6aa084054ced7c67
imadberrabah1-cloud <imad.berrabah1@gmail.com>
2026-01-18 11:51:55 +0100

    Fix Streamlit f-string syntax error 2

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import joblib
-# load the model and 
+
 model = joblib.load("Prb_congestion_model.pkl")
-#stramlit
+
 st.title("Prb congestion")
-st.write("enter the value of CCE congestion ,Trafic and number of users")
-# user input 
-data= st.number_input("CCE congestion ,Trafic and number of users",min_value=0.0,step=1.0)
+st.write("Enter CCE congestion, Traffic and number of users")
+
+# 3 inputs
+charge_CCE = st.number_input("CCE congestion", min_value=0.0, step=1.0)
+Traffic    = st.number_input("Traffic",        min_value=0.0, step=1.0)
+NBR_UE     = st.number_input("Number of users (NBR_UE)", min_value=0.0, step=1.0)
 
 if st.button("Predict"):
-  try:
-      data = [[charge_CCE,Trafic,NBR_UE]]
-      prediction = model.predict(data)
-      st.write(f"predicted cell state :{prediction[0]:.2f}")
-  except Exception as e:
-       st.error(f"Error:{e}")
-
+    try:
+        X = [[charge_CCE, Traffic, NBR_UE]]
+        prediction = model.predict(X)
+        st.write(f"Predicted cell state: {float(prediction[0]):.2f}")
+    except Exception as e:
+        st.error(f"Error: {e}")
       
